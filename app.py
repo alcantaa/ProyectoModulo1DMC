91b8fe9b554834cdd8aef78774921713aee77242
alcantaa <aalcantaralr@gmail.com>
2026-07-26 13:11:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,20 @@ st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
 
 st.image("Python_logo.png")
-st.sidebar.image("DMC.png")         
+st.sidebar.image("DMC.png")   
 
-valor_inicial = st.number_input("ingrese el valor inicial",value=0)
-valor_final = st.number_input("ingrese el valor final",value = 1)
+modulo = st.sidebar.selectbox("Elija un modulo",["Modulo Listas","Modulo Array","Modulo funciones"])
 
-lista_numerica = list(range(valor_inicial,valor_final))
+if modulo == "Modulo Listas":
 
-st.write(lista_numerica)
+  valor_inicial = st.number_input("ingrese el valor inicial",value=0)
+  valor_final = st.number_input("ingrese el valor final",value = 1)
+  lista_numerica = list(range(valor_inicial,valor_final))
+  st.write(lista_numerica)
+
+elif modulo == "Modulo Array":
+  st.write("Estas en el modulo de arreglos")
+
+else 
+  st.write("Estas en el modulo de funciones")
+   
